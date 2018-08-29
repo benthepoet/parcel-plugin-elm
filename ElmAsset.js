0e72f5462bc914c82300e4359a1597342140e1d2
Ben Hanna <benpaulhanna@gmail.com>
2018-08-29 20:51:14 +0000

    Added terser and cleaned up pipeline.

--- ElmAsset.js
+++ ElmAsset.js
@@ -1,24 +1,12 @@
 const process = require('process');
 const Asset = require('parcel-bundler/src/Asset');
 const localRequire = require('parcel-bundler/src/utils/localRequire');
-const terser = require('parcel-bundler/src/transforms/terser');
+const { minify } = require('terser');
 
 class ElmAsset extends Asset {
   constructor(name, options) {
     super(name, options);
     this.type = 'js';
-  }
-
-  getParserOptions() {
-    const parserOptions = {
-      cwd: process.cwd(),
-    };
-    
-    if (this.options.minify) {
-      parserOptions.optimize = true;
-    }
-    
-    return parserOptions;
   }
   
   async collectDependencies() {
@@ -31,14 +19,25 @@ class ElmAsset extends Asset {
   }
   
   async parse() {
-    const options = this.getParserOptions();
     const elm = await localRequire('node-elm-compiler', this.name);
-    const compiled = await elm.compileToString(this.name, options);
     
+    const options = {
+      cwd: process.cwd()
+    };
+    
+    if (this.options.minify) {
+      options.optimize = true;
+    }
+    
+    const compiled = await elm.compileToString(this.name, options);
     this.contents = compiled.toString();
+  }
+
+  async generate() {
+    let output = this.contents;
     
     if (this.options.hmr) {
-      this.contents += `
+      output += `
         (function () {
           if (module.hot) {
             // Disable Hot Module Replacement
@@ -51,19 +50,36 @@ class ElmAsset extends Asset {
         })();
       `;
     }
+    
+    if (this.options.minify) {
+      output = mangle(compress(output));
+    }
+    
+    return {
+      [this.type]: output
+    };
+    
+    function compress(source) {
+        const options = {
+          compress: {
+            keep_fargs: false,
+            pure_funcs: 'F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9'.split(','),
+            pure_getters: true,
+            unsafe: true,
+            unsafe_comps: true
+          }
+        };
+      
+        const { code } = minify(source, options);
+        return code;
+      }
+      
+      function mangle(contents) {
+        const { code } = minify(contents, { mangle: true });
+        return code;
+      }
   }
 
-  async generate() {
-    return {
-      [this.type]: this.outputCode || this.contents
-    };
-  }
-  
-  async transform() {
-    if (this.options.minify) {
-      await terser(this);
-    }
-  }
 }
 
 module.exports = ElmAsset;
